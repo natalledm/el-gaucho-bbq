b9a12b1b56a38e8bbfd94594682dc62e736a6f32
Natalle <natallemoura7@gmail.com>
2022-04-17 17:25:34 +0200

    Test data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// packages
+import { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -5,6 +7,10 @@ import {
   Navigate,
 } from "react-router-dom";
 
+// scripts
+import { getCollection } from "./scripts/fireStoreDB";
+
+// components
 import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
@@ -12,6 +18,22 @@ import MenuPage from "./pages/MenuPage";
 import AdminPage from "./pages/AdminPage";
 
 export default function App() {
+  const [menu, setMenu] = useState([]);
+  const [status, setStatus] = useState(0);
+
+  useEffect(() => {
+    async function loadData(path) {
+      const data = await getCollection(path);
+
+      setMenu(data);
+      setStatus(1);
+    }
+    loadData("categories");
+  }, []);
+
+  if (status === 0) return <p>Loading...</p>;
+  if (status === 2) return <p>Error!</p>;
+
   return (
     <Router>
       <Routes>
