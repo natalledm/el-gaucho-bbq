edd50ad2eb75c80ba06555f5555992b60104fc64
Natalle <natallemoura7@gmail.com>
2022-04-18 17:41:30 +0200

    Category page gets content from the database

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
+// script
+import { getCollection } from "../scripts/fireStoreDB";
+
 export default function CategoryPage() {
-  return <div>Category here</div>;
+  const { category } = useParams();
+
+  const [dishes, setDishes] = useState([]);
+
+  useEffect(() => {
+    async function loadData(path) {
+      const dishesDB = await getCollection(path);
+      setDishes(dishesDB);
+    }
+    loadData(`menu/${category}/content`);
+  }, [category]);
+
+  const dishesElements = dishes.map((item) => {
+    return (
+      <li key={item.id}>
+        <Link to={`/menu/${category}/${item.name}`}>{item.name}</Link>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h1>{category}</h1>
+      <ul>{dishesElements}</ul>
+    </div>
+  );
 }
